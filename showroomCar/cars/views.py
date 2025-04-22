d3b8288aa47bc41dc2feccdf833b55dabd776d5d
xarksyuz <randas.edso@gmail.com>
2025-04-22 19:46:38 +0700

    import model cars

--- showroomCar/cars/views.py
+++ showroomCar/cars/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Cars
+import logging
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 def carList(request):
     """
     View to display the list of cars in the showroom.
     """
-      
+    
     # Render the template with the list of cars
     return render(request, 'cars/index.html')
 
