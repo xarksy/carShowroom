544fec133b285f025b0e339725c2dc0c3ab9b3b8
xarksyuz <randas.edso@gmail.com>
2025-04-22 22:36:06 +0700

    detail works belum pasang listener di car list

--- showroomCar/cars/urls.py
+++ showroomCar/cars/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import carList, create_car
+from .views import carList, create_car, detail_car
 
 urlpatterns = [
     path('',carList,name='carList'),
     path('create/', create_car, name='create_car'),
+    path('detail/<int:car_id>/', detail_car, name='detail_car'),
 ]
