3dcd30ae91a555e5aa006f0fa5709e63133493a1
xarksyuz <randas.edso@gmail.com>
2025-04-22 21:11:51 +0700

    rubah urutan di views

--- showroomCar/cars/views.py
+++ showroomCar/cars/views.py
@@ -14,10 +14,10 @@ def carList(request):
     # Render the template with the list of cars
     return render(request, 'cars/index.html', context)
 
-def carDetail(request, car_id):
+def addCar(request):
     pass
 
-def addCar(request):
+def carDetail(request, car_id):
     pass
 
 def updateCar(request, car_id):
