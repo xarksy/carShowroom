78f92b9b49dc0bea7350dea424ef2b5c8c67b426
xarksyuz <randas.edso@gmail.com>
2025-04-23 22:23:13 +0700

    fix perhitungan simulasi

--- showroomCar/cars/static/cars/js/hpp_bank.js
+++ showroomCar/cars/static/cars/js/hpp_bank.js
@@ -6,9 +6,13 @@ const cashInput = document.getElementById('cash');
     const calculateButton = document.getElementById('hitung');
     const resetButton = document.getElementById('reset_hitungan');
 
-    const resultDiv = document.createElement('div');
-    resultDiv.id = 'simulation_result';
-    document.getElementById('credit_simulation').appendChild(resultDiv);
+    // const resultDiv = document.createElement('div');
+    // resultDiv.id = 'simulation_result';
+    // document.getElementById('credit_simulation').appendChild(resultDiv);
+
+    const angsuranBulanan = document.getElementById('angsuran_bulanan');
+    const totalPembayaran = document.getElementById('total_pembayaran');
+    const totalBunga = document.getElementById('total_bunga');
 
     calculateButton.addEventListener('click', function (event) {
         event.preventDefault();
@@ -23,15 +27,19 @@ const cashInput = document.getElementById('cash');
             const totalPayment = monthlyPayment * tenor;
             const totalInterest = totalPayment - cash;
 
-            resultDiv.innerHTML = `
-                <h3>Hasil Simulasi</h3>
-                <p>Uang Muka: Rp${cash.toLocaleString()}</p>
-                <p>Tenor: ${tenor} bulan</p>
-                <p>Bunga: ${interest}% per tahun</p>
-                <p>Angsuran per bulan: Rp${monthlyPayment.toLocaleString()}</p>
-                <p>Total Pembayaran: Rp${totalPayment.toLocaleString()}</p>
-                <p>Total Bunga: Rp${totalInterest.toLocaleString()}</p>
-            `;
+            angsuranBulanan.innerText = `Rp${monthlyPayment.toLocaleString()}`;
+            totalPembayaran.innerText = `Rp${totalPayment.toLocaleString()}`;
+            totalBunga.innerText = `Rp${totalInterest.toLocaleString()}`;
+
+            // resultDiv.innerHTML = `
+            //     <h3>Hasil Simulasi</h3>
+            //     <p>Uang Muka: Rp${cash.toLocaleString()}</p>
+            //     <p>Tenor: ${tenor} bulan</p>
+            //     <p>Bunga: ${interest}% per tahun</p>
+            //     <p>Angsuran per bulan: Rp${monthlyPayment.toLocaleString()}</p>
+            //     <p>Total Pembayaran: Rp${totalPayment.toLocaleString()}</p>
+            //     <p>Total Bunga: Rp${totalInterest.toLocaleString()}</p>
+            // `;
         }
     });
 
@@ -39,6 +47,8 @@ const cashInput = document.getElementById('cash');
         cashInput.value = '';
         tenorInput.value = 12; // Set default tenor to 12 months
         interestInput.value = 5; // Set default interest to 5%
-        resultDiv.innerHTML = ''; // Clear previous results
+        angsuranBulanan.innerText = '-'
+        totalPembayaran.innerText = '-';
+        totalBunga.innerText = '-';
     });
 });
