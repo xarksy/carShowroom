be48c809d096dd0c1265cb71bb213ddb3cb7d623
xarksyuz <randas.edso@gmail.com>
2025-04-23 10:16:02 +0700

    tambah listener untuk masuk detail

--- showroomCar/cars/static/cars/js/custom.js
+++ showroomCar/cars/static/cars/js/custom.js
@@ -1,36 +1,35 @@
 document.getElementById('view-grid').addEventListener('click', function() {
-    document.getElementById('car-container').setAttribute('data-view', 'grid');
-    document.querySelectorAll('.car-item').forEach(function(item) {
-        item.classList.add('d-none');
-        if (item.classList.contains('grid-view')) {
-        item.classList.remove('d-none');
-        }
-    });
-    });
+document.getElementById('car-container').setAttribute('data-view', 'grid');
+document.querySelectorAll('.car-item').forEach(function(item) {
+    item.classList.add('d-none');
+    if (item.classList.contains('grid-view')) {
+    item.classList.remove('d-none');
+    }
+});
+});
 
-    document.getElementById('view-list').addEventListener('click', function() {
-    document.getElementById('car-container').setAttribute('data-view', 'list');
-    document.querySelectorAll('.car-item').forEach(function(item) {
-        item.classList.add('d-none');
-        if (item.classList.contains('list-view')) {
-        item.classList.remove('d-none');
-        }
-    });
-    });
-
-    // Select the button using its class
-    const closeButton = document.querySelector('.close');
-
-    // Add a click event listener
-    closeButton.addEventListener('click', () => {
-        console.log('Close button clicked!');
-        // Add your logic here, e.g., hiding a modal or alert
-    });
-
-    // Close button functionality
+document.getElementById('view-list').addEventListener('click', function() {
+document.getElementById('car-container').setAttribute('data-view', 'list');
+document.querySelectorAll('.car-item').forEach(function(item) {
+item.classList.add('d-none');
+if (item.classList.contains('list-view')) {
+item.classList.remove('d-none');
+}
+});
+});
+    
+function attachListeners() {        
     document.querySelectorAll('.close').forEach(function(button) {
         button.addEventListener('click', function() {
             const card = this.closest('.car-item');
             card.remove();
         });
-    });
+    });        
+}
+
+
+window.onload = function() {
+    attachListeners();      
+}
+
+    
